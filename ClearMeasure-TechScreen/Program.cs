c89a5495e6e334f1745cd817577629183c5bcd68
Joe Reynolds <jreynolds@infocraft.net>
2013-12-06 14:50:10 -0600

    Initial Commit

---
+++ ClearMeasure-TechScreen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OurCoolThings;
+
+namespace ClearMeasure_TechScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = 1;
+            var lastNumber = 1000000000;
+            var rules = new List<ReplacementRules>()
+            {
+                new ReplacementRules(){Divisors = new List<int>(){3}, Label = "Fizz"},
+                new ReplacementRules(){Divisors = new List<int>(){5}, Label = "Buzz"},
+                new ReplacementRules(){Divisors = new List<int>(){3,5}, Label = "FizzBuzz"},
+            };
+            //var fizzBuzz = Utilities.FizzBuzzer(firstNumber, lastNumber);
+            foreach (var item in Utilities.FizzBuzzer(firstNumber,  lastNumber, rules))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OurCoolThings/Utilities.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OurCoolThings
+{
+    public class Utilities
+    {
+
+
+        public static IEnumerable<string> FizzBuzzer(int firstNumber, int lastNumber, List<ReplacementRules> replacementRules)
+        {
+            
+            for (int i = firstNumber; i < lastNumber+1; i++)
+            {
+                string outPut = i.ToString();
+
+                foreach (var replacementRule in replacementRules)
+                {
+                    if (replacementRule.Divisors.Count == 1)
+                    {
+                        if (i%replacementRule.Divisors.First() == 0) outPut = replacementRule.Label;
+                    }
+                    else
+                    {
+                        var success = false;
+                        foreach (var divisor in replacementRule.Divisors)
+                        {
+                            if(i%divisor!=0) 
+                            {
+                                success=false;
+                                break;
+                            }
+                            success = true;
+                        }
+                        if (success) outPut = replacementRule.Label;
+                    }
+                        
+                }
+
+                //if (i%3 == 0) outPut = "Fizz";
+                //if (i%5 == 0) outPut = "Buzz";
+                //if (i%3 == 0 && i%5 == 0) outPut = "FizzBuzz";
+                yield return outPut;
+            }
+        }
+    }
+
+    public class ReplacementRules
+    {
+        public ReplacementRules()
+        {
+            Divisors = new List<int>();
+            Label = string.Empty;
+        }
+        public List<int> Divisors { get; set; }
+        public string Label { get; set; }
+    }
+}
